178cb12a9d5031a5cbd947944efa31d500ae3c18
vickyone0 <vigneshs0.618@gmail.com>
2025-06-18 04:40:48 -0400

    websocket connection

--- src/main.rs
+++ src/main.rs
@@ -6,25 +6,59 @@ pub mod repository;
 pub mod auth;
 pub mod middleware;
 
-use actix_web::{App, HttpServer, web};
+use actix_web::{web, App, HttpRequest, HttpServer};
 
 use database::establish_connection;
 
 
+// #[actix_web::main]
+// async fn main() -> std::io::Result<()> {
+
+
+
+//             let db_pool = establish_connection().await;
+//             HttpServer::new(move || {
+//                 App::new()
+//                     .app_data(web::Data::new(db_pool.clone()))
+//                     .configure(routes::config)
+//             })
+//             .bind(("127.0.0.1",8080))?
+//             .run()
+//             .await
+// }
+
+
+//use actix_web::{web, App, HttpServer, Responder};
+use actix_web_actors::ws;
+
+struct MyWebSocket;
+
+impl StreamHandler<Result<ws::HttpMessage, ws::ProtocolError>> for MyWebSocket {
+    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+        match msg {
+            Ok(ws::Message::Text(text)) => ctx.text(text),
+            Ok(ws::Message::Close(reason)) => ctx.close(reason),
+            _ => (),
+            
+        }
+    }
+}
+
+async fn websocket_route(req: HttpRequest, stream: web::Payload) -> impl Responder {
+    ws::start(MyWebSocket {}, &req, stream)
+}
+
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-
-
-
-            let db_pool = establish_connection().await;
-            HttpServer::new(move || {
-                App::new()
-                    .app_data(web::Data::new(db_pool.clone()))
-                    .configure(routes::config)
-            })
-            .bind(("127.0.0.1",8080))?
-            .run()
-            .await
+    HttpServer::new(|| {
+        App::new()
+            .route("/ws", web::get().to(websocket_route))  
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
+
 
 
